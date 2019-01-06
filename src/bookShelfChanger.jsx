a35764f1517b6a119bb28be68bb348c0722d4e2d
Laura Degeratu <loveable_koneko@yahoo.com>
2019-01-06 22:48:49 +0100

    get props, display current shelf, update on click

--- src/bookShelfChanger.jsx
+++ src/bookShelfChanger.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default class BookShelfChanger extends React.Component {
+    handleChange(event) {
+        const { bookId, displayClick } = this.props;
+        displayClick({bookId: bookId, value:event.target.value});
+    }
+    
     render() {
+        const { currentShelf } = this.props;
         return (
             <div className="book-shelf-changer">
-                <select>
+                <select value={currentShelf ? currentShelf : "none"} onChange={event => this.handleChange(event)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
